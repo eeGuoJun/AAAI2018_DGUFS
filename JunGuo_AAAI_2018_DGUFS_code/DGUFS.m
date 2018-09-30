c7b6f7694ba34958cc4717504bd5210c191cdfc9
eeGuoJun <eeguojun@foxmail.com>
2018-09-30 11:40:40 +0800

    Add files via upload

--- JunGuo_AAAI_2018_DGUFS_code/DGUFS.m
+++ JunGuo_AAAI_2018_DGUFS_code/DGUFS.m
@@ -1,11 +1,5 @@
 function [Y,L,V,Label] = DGUFS(X,nClass,S,alpha,beta,nSel) 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Descripion: 
-% This function implements the whole algorithm of our paper
-% Jun Guo and Wenwu Zhu, "Dependence Guided Unsupervised 
-% Feature Selection," In AAAI 2018.
-%   -Source code version 1.0  2018/03/01 by Jun Guo
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input:
 %       X       -each column is an original sample
 %       nClass  -the number of clusters
@@ -18,18 +12,6 @@ function [Y,L,V,Label] = DGUFS(X,nClass,S,alpha,beta,nSel)
 %       L       -kernel matrix L = V'*V
 %       V       -each column is a one-hot label
 %       Label   -a label vecter whose size is nSmp*1 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Please cite our work if you find the code helpful.
-% @inproceedings{JunGuo_AAAI_2018_DGUFS,
-%   author = {J. Guo and W. Zhu},
-%   title = {Dependence Guided Unsupervised Feature Selection},
-%   booktitle = {Proc. AAAI Conf. Artificial Intell. (AAAI)},
-%   address = {New Orleans, Louisiana},
-%   pages = {2232-2239},
-%   month = {Feb.},
-%   year = {2018}
-% }
-% If you have problems, please contact us at eeguojun@outlook.com
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
